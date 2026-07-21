da5c8ee32aed76eff8e7a4e779e558142f41d6b0
Pedro-Pires-fag <pebpires@minha.fag.edu.br>
2026-07-21 10:03:42 -0300

    WIP: adding store cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { GraphicComponent } from "./graphic/graphic.component";
+import { CardComponent } from "./cards/card.component/card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, GraphicComponent],
+  imports: [RouterOutlet, SidebarComponent, GraphicComponent, CardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

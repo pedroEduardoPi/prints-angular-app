9e3d1e5831c0fa3a43bb5abed0e153a2eb74002f
ti07.mtz <ti07.mtz@superirani.local>
2026-07-21 14:03:11 -0300

    WIK: adding store charts

--- src/app/graphic/util/graphic.util.ts
+++ src/app/graphic/util/graphic.util.ts
@@ -1,28 +1,28 @@
 import { PrintData } from "../../print/PrintDataModel";
 
 export function printsPerDay(prints: PrintData[]): Map<string, number> {
-    
-    if(prints.length === 0)
+
+    if (prints.length === 0)
         return new Map<string, number>();
 
-    const counts = new Map<string, number>(); 
+    const counts = new Map<string, number>();
     const firstDate = new Date(prints[0].dataImpressao!);
     const year = firstDate.getFullYear();
     const month = firstDate.getMonth();
     const totalDays = daysInMonth(year, month);
 
-    for (let day = 1; day <= totalDays; day++){
+    for (let day = 1; day <= totalDays; day++) {
         const label = formatterLabel(day, month + 1);
 
         counts.set(label, 0);
     }
 
     for (const print of prints) {
-      if (!print.dataImpressao) continue;
+        if (!print.dataImpressao) continue;
 
-      const date = new Date(print.dataImpressao);
-      const label = formatterLabel(date.getDate(), date.getMonth() + 1);
-      counts.set(label, (counts.get(label) ?? 0) + 1);
+        const date = new Date(print.dataImpressao);
+        const label = formatterLabel(date.getDate(), date.getMonth() + 1);
+        counts.set(label, (counts.get(label) ?? 0) + 1);
     }
 
     return counts;
@@ -34,4 +34,8 @@ function daysInMonth(year: number, month: number): number {
 
 export function formatterLabel(day: number, month: number): string {
     return `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}`;
+}
+
+export function filterStore(prints: PrintData[],): PrintData[] {
+    return prints.filter(p => p.enderecoDispositivo!.startsWith("172.17.23."));
 }
